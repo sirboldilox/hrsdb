3eab237477b7fd1425ec1e08ef6a3da69d4f9ab8
Matt Parker <m.parker-12@student.lboro.ac.uk>
2016-03-25 14:10:24 +0000

    Updated setup.py, increment version to 0.2

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from setuptools import setup, find_packages
 
 setup(
     name='HRSDB',
-    version='0.1',
-    description='Health Record System Database',
+    version='0.2',
     author='Matt Parker',
     author_email='m.parker-12@student.lboro.ac.uk',
+    description='Health Record System Database',
+    license = "MIT",
+
+    classifiers = [
+        "Development Status :: 3 - Alpha"
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux"
+    ],
 
     packages=find_packages(),
-    scripts=[
-        'scripts/hrsdb_init',
-        'scripts/hrsdb_add_patient',
+    include_package_data = True,
+    zip_safe = False,
+
+    install_requires=[
+        'Flask',
+        'Flask-RESTful',
+        'SQLALchemy'
     ],
+
     entry_points={
         'console_scripts': [
-            'hrsdb_http = hrsdb.__main__:main'
+            'hrsdb = hrsdb.__main__:main'
         ]
     }
 )
